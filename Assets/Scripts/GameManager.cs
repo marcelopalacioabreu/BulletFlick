5b685f82df42c0fc73caedec9cd74f5f28592576
shreychowdhary <shrey.chowdhary1@gmail.com>
2017-12-25 09:49:13 -0500

    Setup spawn points

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,21 @@ using UnityEngine.SceneManagement;
 public class GameManager : Photon.PunBehaviour {
 
     [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject spawnPointHolder;
+
+    List<GameObject> spawnPoints;
+
+    void Awake () {
+        spawnPoints = new List<GameObject>();
+    }
 
 	// Use this for initialization
 	void Start () {
-        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0,10,0), Quaternion.identity, 0);
-	}
+        foreach (Transform child in spawnPointHolder.transform) {
+            spawnPoints.Add(child.gameObject);
+        }
+        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 10, 0), Quaternion.identity, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
