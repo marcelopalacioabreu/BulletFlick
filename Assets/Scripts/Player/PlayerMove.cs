7aeeb043e5057e0eacda70a6c63204b0c84c888f
shreychowdhary <shrey.chowdhary1@gmail.com>
2018-01-03 10:44:33 -0500

    Restructre scripts

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -16,8 +16,12 @@ namespace BulletFlick {
         private CharacterController controller;
         private Vector3 moveDirection;
 
+        private bool hasDoubleJumped;
+
         void Start () {
             controller = GetComponent<CharacterController>();
+            hasDoubleJumped = false;
+            moveDirection = Vector3.zero;
         }
         
         void Update () {
@@ -25,8 +29,14 @@ namespace BulletFlick {
                 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
                 moveDirection = playerCamera.transform.TransformDirection(moveDirection);
                 moveDirection *= speed;
-                if (Input.GetButton("Jump")) {
-                    moveDirection.y = jumpSpeed;
+                hasDoubleJumped = false;
+            }
+            if (Input.GetButtonDown("Jump") && (!hasDoubleJumped || controller.isGrounded)) {
+                Debug.Log("Jump");
+                moveDirection.y = jumpSpeed;
+                if (!controller.isGrounded) {
+                    Debug.Log("Double");
+                    hasDoubleJumped = true;
                 }
             }
             moveDirection.y -= Gravity * Time.deltaTime;
