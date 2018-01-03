5d706563b8163b438ec86ff43ce1ffedefc0dc45
shreychowdhary <shrey.chowdhary1@gmail.com>
2018-01-03 13:02:37 -0500

    Added map choosing

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+namespace BulletFlick {
+    public class Launcher : Photon.PunBehaviour {
+
+        [SerializeField] private byte maxPlayers = 4;
+
+        [SerializeField] private GameObject mapContent;
+
+        [SerializeField] private GameObject mapOptionPrefab;
+
+        private string gameVersion = "1";
+
+        private int mapIndex;
+
+        void Awake () {
+            PhotonNetwork.autoJoinLobby = false;
+            PhotonNetwork.automaticallySyncScene = true;
+        }
+
+        // Use this for initialization
+        void Start () {
+            //start at 1 to skip launcher
+            for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++) {
+                GameObject mapOption = Instantiate(mapOptionPrefab);
+                mapOption.transform.parent = mapContent.transform;
+                mapOption.GetComponent<RectTransform>().anchoredPosition 
+                    = new Vector2(0, -50 * (i - 1));
+
+                string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+                Debug.Log(scenePath);
+                string sceneName = scenePath.Substring(scenePath.LastIndexOf("/") + 1,
+                    scenePath.LastIndexOf(".")-(scenePath.LastIndexOf("/") + 1));
+
+                mapOption.GetComponentInChildren<Text>().text = sceneName;
+                //need indexCopy for delegate, otherwise runs last i value
+                int indexCopy = i;
+                mapOption.GetComponentInChildren<Button>().onClick.AddListener(delegate { Play(indexCopy); });
+            }
+
+            Connect();
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+
+        public void Connect () {
+            if (!PhotonNetwork.connected) { 
+                PhotonNetwork.ConnectUsingSettings(gameVersion);
+            }
+        }
+
+        public void Play(int index) {
+            Debug.Log(index);
+            mapIndex = index;
+            if (PhotonNetwork.connectedAndReady) {
+                Hashtable roomProperties = new Hashtable() { { "map", mapIndex } };
+                PhotonNetwork.JoinRandomRoom(roomProperties, maxPlayers);
+            }
+        }
+
+        public override void OnJoinedRoom () {
+            SceneManager.LoadScene(mapIndex);
+        }
+
+        public override void OnPhotonRandomJoinFailed (object[] codeAndMsg) {
+            string[] propForLobby = { "map" };
+            RoomOptions roomOptions = new RoomOptions {
+                MaxPlayers = maxPlayers,
+                CustomRoomPropertiesForLobby = propForLobby,
+                CustomRoomProperties = new Hashtable() { {"map", mapIndex} }
+            };
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = maxPlayers }, null);
+        }
+    }
+}
